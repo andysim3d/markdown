30cc178f0302f6b4ea3ddd744ffb0ffffc44918c
jinglongwu <silence.jingle@gmail.com>
2020-08-07 18:20:50 -0400

    parent 4eb483f099379229257876a0b9e148eeacab1d6d author jinglongwu <silence.jingle@gmail.com> 1596838850 -0400 committer jinglongwu <silence.jingle@gmail.com> 1596838955 -0400
    parent 4eb483f099379229257876a0b9e148eeacab1d6d
    author jinglongwu <silence.jingle@gmail.com> 1596838850 -0400
    committer jinglongwu <silence.jingle@gmail.com> 1596838850 -0400
    
    rebase merge
    
    update script
    
    test travis with pylint
    
    add parse() for Bold + Bold and Italic
    
    add not match case
    
    clean up
    
    add des, test
    
    Header parser (#7)
    
    * Add img block and quote paragraph. Test included.
    
    Co-authored-by: jinglongwu <38930967+jinglongwu@users.noreply.github.com>

--- bold_test.py
+++ bold_test.py
@@ -12,3 +12,44 @@ from bold import BoldBlock
 def test_render(content, html):
     bold = BoldBlock(content)
     assert html == bold.render()
+
+
+@pytest.mark.parametrize(
+    "content, start, end, sub_content",
+    [
+        # Bold and Italic (Either head or tail has 3 asterisks and total asterisks > 6)
+        ('***test***', 0, 10, '*test*'),
+        ('****test***', 0, 11, '**test*'),
+        ('***test****', 0, 11, '*test**'),
+        ('*****test***', 0, 12, '***test*'),
+        ('***test*****', 0, 12, '*test***'),
+        # Bold only (Otherwise from above, head and tail has at least 2 asterisks)
+        ('**test**', 0, 8, 'test'),
+        ('***test**', 0, 9, '*test'),
+        ('**test***', 0, 9, 'test*')
+    ]
+)
+def test_parse(content, start, end, sub_content):
+    (start_index, end_index, bold_block) = BoldBlock.parse(content)
+    assert start_index == start
+    assert end_index == end
+    assert bold_block._content == sub_content
+
+
+@pytest.mark.parametrize(
+    "content",
+    [
+        # not match
+        ('test'),
+        ('*test'),
+        ('*test*'),
+        ('*test**'),
+        ('**test*'),
+        ('*_test_*'),
+    ]
+)
+def test_parse_not_match(content):
+    (start_index, end_index, bold_block) = BoldBlock.parse(content)
+    assert start_index == -1
+    assert end_index == -1
+    assert bold_block is None
