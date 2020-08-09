b687dfe88aaefc6e140822a861f2d078680468ae
VCneverdie <vivienne.cuiy@gmail.com>
2020-08-09 00:23:50 -0400

    add parse for LinkBlock

--- link_block.py
+++ link_block.py
@@ -1,3 +1,4 @@
+import re
 from block import Block
 
 
@@ -8,3 +9,24 @@ class LinkBlock(Block):
 
     def render(self) -> str:
         return f"<a href='{self._url}'>{self.content()}</a>"
+
+    @staticmethod
+    def parse(content) -> (int, int, 'LinkBlock'):
+        """Parse a link content, and return (begin, end, LinkBlock) if parseable,
+        or (-1, -1, None) that no such a element
+        """
+        pattern = r'(^|[^\\])\[(.*[^\\])\]\((.*[^\\])\)'
+
+        link_match = re.search(pattern, content)
+        if link_match:
+            start = link_match.start()
+            if link_match.group(1):
+                # escape case
+                start += 1
+            end = link_match.end()
+            url = link_match.group(3)
+            content = link_match.group(2)
+            url = url if url else ""
+            content = content if content else ""
+            return (start, end, LinkBlock(content, url))
+        return (-1, -1, None)
