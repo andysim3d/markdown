548f284ee9e068e12f9b91573060efc671396925
VCneverdie <vivienne.cuiy@gmail.com>
2020-08-09 01:36:09 -0400

    add parse for wave

--- strikethrough_block_test.py
+++ strikethrough_block_test.py
@@ -9,3 +9,31 @@ from strikethrough_block import StrikethroughBlock
 def test_strikethrough_render(content, html):
     strikethrough_block = StrikethroughBlock(content)
     assert html == strikethrough_block.render()
+
+
+@pytest.mark.parametrize("content, expected_start, expected_end, html", [
+    ("~testing text~", 0, 14, "<del>testing text</del>"),
+    ("~~testing text~~", 0, 16, "<del>testing text</del>"),
+    ("\\~~testing text~", 2, 16, "<del>testing text</del>"),
+    ("~testing text~\\~", 0, 14, "<del>testing text</del>"),
+    ("~testing text\\~~", 0, 16, "<del>testing text\\~</del>"),
+])
+def test_strikethrough_parse(content, expected_start, expected_end, html):
+    start, end, strikethrough_block = StrikethroughBlock.parse(content)
+    assert expected_start == start
+    assert expected_end == end
+    assert strikethrough_block is not None
+    assert html == strikethrough_block.render()
+
+
+@pytest.mark.parametrize("content", [
+    ("~test"),
+    ("~~test"),
+    ("~~test~"),
+    ("~~~test~~~"),
+    ("~~~test")
+])
+def test_strikethrough_parse_failed(content):
+    start, end, strikethrough_block = StrikethroughBlock.parse(content)
+    assert -1 == start == end
+    assert strikethrough_block is None
