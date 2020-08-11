24c0b9f30e86c9b5947fa07a8584ec60e4dbcec1
jinglongwu <silence.jingle@gmail.com>
2020-08-11 13:07:05 -0400

    Parse() for Horizontal Rule

--- horizontal_rule_test.py
+++ horizontal_rule_test.py
@@ -1,6 +1,54 @@
+import pytest
 from horizontal_rule import HorizontalRule
 
 
 def test_render():
     horiz_rule = HorizontalRule('')
     assert horiz_rule.render() == '<hr>'
+
+
+@pytest.mark.parametrize(
+    "content, start, end",
+    [
+        ('***', 0, 3),
+        ('____', 0, 4),
+        ('-----', 0, 5),
+        (
+            '''---
+            abc
+            ''', 0, 3
+        ),
+        (
+            '''abc
+*****
+            ''', 4, 9
+        ),
+    ]
+)
+def test_parse(content, start, end):
+    (start_index, end_index, hori_rule) = HorizontalRule.parse(content)
+    assert start_index == start
+    assert end_index == end
+    assert hori_rule._content == ''
+
+
+@pytest.mark.parametrize(
+    "content",
+    [
+        # not match
+        ('**'),
+        ('*_-'),
+        ('\---'),
+        ('a___'),
+        ('*****a'),
+        ('***a**'),
+        (' ------'),
+        ('______ '),
+        ('*** ***'),
+    ]
+)
+def test_parse_not_match(content):
+    (start_index, end_index, hori_rule) = HorizontalRule.parse(content)
+    assert start_index == -1
+    assert end_index == -1
+    assert hori_rule is None
