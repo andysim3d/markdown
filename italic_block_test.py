6d6158de4833930d6de4253898479b673d9cbe0e
VCneverdie <vivienne.cuiy@gmail.com>
2020-08-19 23:53:29 -0400

    add more tests and use raw string

--- italic_block_test.py
+++ italic_block_test.py
@@ -11,15 +11,20 @@ def test_italic_render(content, html):
     assert html == italic_block.render()
 
 
-@pytest.mark.parametrize("content, html", [
-    ("*testing text*", "<em>testing text</em>"),
-    ("_testing text_", "<em>testing text</em>"),
-    ("*testing text**", "<em>testing text*</em>")
+@pytest.mark.parametrize("content, expected_start, expected_end, html", [
+    ("*testing text*", 0, 14, "<em>testing text</em>"),
+    ("_testing text_", 0, 14, "<em>testing text</em>"),
+    ("*testing text**", 0, 15, "<em>testing text*</em>"),
+    ("*_testing text_*", 0, 16, "<em>_testing text_</em>"), # the final output is <em>testing text</em>
+    (r"\_*testing text\_*", 2, 18, r"<em>testing text\_</em>"),
+    (r"\**testing text*", 2, 16, "<em>testing text</em>"),
+    (r"\**testing text*\*", 2, 16, "<em>testing text</em>"),
+    (r"*\*testing text*\*", 0, 16, r"<em>\*testing text</em>")
 ])
-def test_italic_parse(content, html):
+def test_italic_parse(content, expected_start, expected_end, html):
     start, end, italic_block = ItalicBlock.parse(content)
-    assert start == 0
-    assert end == len(content)
+    assert start == expected_start
+    assert end == expected_end
     assert italic_block is not None
     assert html == italic_block.render()
 
@@ -27,7 +32,9 @@ def test_italic_parse(content, html):
 @pytest.mark.parametrize("content", [
     ("*testing text"),
     ("*testing text_"),
-    ("\\*testing text*"),
+    (r"\*testing text*"),
+    (r"\*testing text\*"),
+    (r"\**testing text\*")
 ])
 def test_italic_parse_failed(content):
     start, end, italic_block = ItalicBlock.parse(content)
