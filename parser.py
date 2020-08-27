3c2687a77fb8cd703d34032e51c653d42a2f9b14
jinglongwu <silence.jingle@gmail.com>
2020-08-27 14:31:59 -0400

    Draft for Element Tree

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import sys
+from element import Element
 from paragraph import TextParagraph
+from block import TextBlock
 
 
 class AbstractParser(object):
@@ -46,14 +48,39 @@ class ParagraphParser(AbstractParser):
         return content[end:]
 
 
+def link_parent_and_child(parent, child):
+    child.set_parent(parent)
+    parent.add_child(child)
+
 class BlockParser(AbstractParser):
     def __init__(self):
         super().__init__()
-        self._blocks = []
 
-    def parse(self, content):
+    def _invoke_parsers(self, parent, content):
+        start_index = len(content)
+        end_index = len(content)
+        final_element = TextBlock(content)
+        for parser in self._parsers:
+            begin, end, element = parser(content)
+            if begin != 0 and begin < start_index:
+                start_index, end_index, final_element = begin, end, element
+        if start_index != 0 and start_index != len(content):
+            child = TextBlock(content[0, start_index])
+            link_parent_and_child(parent, child)
+        link_parent_and_child(parent, final_element)
+        return content[end:]
+
+    def parse(self, content, root=None):
         '''
         Invoke all parsers in self._parsers to try to parse the content.
         Return unparsed part.
         '''
-        # TODO: implement it.
+        if root is None:
+            root = Element(content)
+        cur_content = content
+        while len(cur_content) > 0:
+            cur_content = self._invoke_parsers(root, cur_content)
+
+        for child in root.children():
+            if not isinstance(child, TextBlock):
+                self.parse(child.content(), child)
