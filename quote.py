43056b37f1f84283192ca3e7a1f7ec6dafa6388d
Andy Zhang <andysim3d@gmail.com>
2020-09-08 11:55:32 -0400

    fix test code

--- quote.py
+++ quote.py
@@ -1,5 +1,5 @@
+import re
 from paragraph import Paragraph
-
 class QuoteParagraph(Paragraph):
     """Element decorated by '>'.
     """
@@ -7,4 +7,19 @@ class QuoteParagraph(Paragraph):
         super().__init__(content)
 
     def render(self) -> str:
-        return "<blockquote> {} </blockquote>".format(self.inner_text())
+        return "<blockquote>{}</blockquote>".format(self.inner_text())
+
+    @staticmethod
+    def parse(content) -> (int, int, 'QuoteParagraph'):
+        """Parse a quote content, and return (begin, end, QuoteParagraph) if parseable,
+        or (-1, -1, None) that no such a element
+        """
+        pattern = r'^[ ]*\>[ ]*(.*)$'
+
+        quote_match = re.search(pattern, content)
+        if quote_match:
+            start = 0
+            end = len(content)
+            content = quote_match.group(1)
+            return (start, end, QuoteParagraph(content))
+        return (-1, -1, None)
