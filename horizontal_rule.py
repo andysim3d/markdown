1730c7a4b1f5d2dcf0d48a4514b35c6f50c74596
jinglongwu <silence.jingle@gmail.com>
2020-09-20 15:24:00 -0400

    HorizontalRule add multiline, fix pytest warning

--- horizontal_rule.py
+++ horizontal_rule.py
@@ -19,8 +19,8 @@ class HorizontalRule(Paragraph):
         Note, the index of start and end follow the convention of [start, end)
         Return (-1, -1, None) if no such match found.
         '''
-        pattern = r'^ *(\*{3,}|-{3,}|_{3,}) *$'
-        matches = re.search(pattern, content)
+        pattern = r'^\s*(\*{3,}|-{3,}|_{3,})\s*$'
+        matches = re.search(pattern, content, re.MULTILINE)
         if matches:
             return (matches.start(1), matches.end(1), HorizontalRule(''))
         else:
