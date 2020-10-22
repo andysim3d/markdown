8bd6b5d1eea69f9112ba0ff312fbf8a885e5b656
VCneverdie <vivienne.cuiy@gmail.com>
2020-10-21 21:34:47 -0400

    refine regex

--- src/parser/list_paragraph_parser.py
+++ src/parser/list_paragraph_parser.py
@@ -4,17 +4,19 @@ from ..blocks import OrderedList, UnorderedList
 
 def replace_leading_tabs(content, leading_len) -> str:
     # replace tab with 4 spaces
-    return content[:leading_len].replace('\t', 4 * ' ')+content[leading_len:]
+    return content[:leading_len].replace('\t', 4 * ' ') + content[leading_len:]
+
 
 def parse_ordered_list(content) -> (int, int, 'OrderedList'):
     """Parse a list paragraph, and return (begin, end, OrderedList) if parseable,
     or (-1, -1, None) that no such a element
     """
-    pattern = r'^([ \t]*)\d\.[ ]+(.*)$\n*'
+    pattern = r'^([ \t]*)\d\.[ ]+.*$\n*'
     matches = re.search(pattern, content, re.MULTILINE)
     if matches:
         new_content = replace_leading_tabs(content, len(matches.group(1)))
-        new_matches = re.search(pattern, new_content, re.MULTILINE)
+        new_pattern = r'^([ ]*)\d\.[ ]+(.*)$\n*'
+        new_matches = re.search(new_pattern, new_content, re.MULTILINE)
         return (
             new_matches.start(), new_matches.end(), OrderedList(
                 new_matches.group(2), len(new_matches.group(1))))
@@ -30,7 +32,8 @@ def parse_unordered_list(content) -> (int, int, 'UnorderedList'):
     matches = re.search(pattern, content, re.MULTILINE)
     if matches:
         new_content = replace_leading_tabs(content, len(matches.group(1)))
-        new_matches = re.search(pattern, new_content, re.MULTILINE)
+        new_pattern = r'^([ ]*)(?:\*|\-|\+)[ ]+(.*)$\n*'
+        new_matches = re.search(new_pattern, new_content, re.MULTILINE)
         return (new_matches.start(), new_matches.end(),
                 UnorderedList(new_matches.group(2), len(new_matches.group(1))))
     return (-1, -1, None)
