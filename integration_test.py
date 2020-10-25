ac89721a6ddeaf7fe5c4f5dc5c8cf1400737862e
Andy Zhang <andysim3d@gmail.com>
2020-10-24 20:36:12 -0400

    fix lint

--- integration_test.py
+++ integration_test.py
@@ -1,8 +1,9 @@
 import os
 import glob
+import pytest
+
 import src.parser.parser as parser
 import src.render.html_render as render
-import pytest
 
 
 def test():
@@ -10,10 +11,10 @@ def test():
         if file.endswith(".md"):
             full_path = (os.path.join(os.getcwd(), "md_to_html", file))
             parsed_html = ""
-            with open(full_path, 'r') as r:
-                content = r.read()
+            with open(full_path, 'r') as md_file:
+                content = md_file.read()
                 root = parser.parse_md_to_ast(content)
                 parsed_html = render.render(root)
-            with open(full_path.replace(".md", ".html"), 'r') as r:
-                content = r.read()
-                assert content == parsed_html
+            with open(full_path.replace(".md", ".html"), 'r') as html_file:
+                html_content = html_file.read()
+                assert html_content == parsed_html
