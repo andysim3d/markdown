196db1d0346a625a038cc7f4218cdd5c746170ca
Andy Zhang <andysim3d@gmail.com>
2020-11-14 14:15:51 -0500

    enrich readme

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
 setup(
     name='lightmd',
-    version='0.0.1',
+    version='0.0.1.1',
     description='This is a light-weighted, purly python made markdown parser and render',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='andysim3d, jinglongwu, VCneverdie, shawn-lo',
     url='https://github.com/andysim3d/markdown',
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
