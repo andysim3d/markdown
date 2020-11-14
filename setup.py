e3050934860a7586b5f0e8c44c603ff4529ef06e
Andy Zhang <andysim3d@gmail.com>
2020-11-14 14:05:50 -0500

    rename project to lightmd

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
 setup(
-    name='pymd',
+    name='lightmd',
     version='0.0.1',
-    description='This is a purly python markdown parser and render',
+    description='This is a light-weighted, purly python made markdown parser and render',
     author='andysim3d, jinglongwu, VCneverdie, shawn-lo',
     url='https://github.com/andysim3d/markdown',
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # packages=find_packages(exclude=('*test*')),
-    # packages= ['src/render', 'src/blocks', 'src/parser'], 
-    package_dir={'pymd': 'pymd'},
+    package_dir={'lightmd': 'lightmd'},
     license="GPL-3.0 License",
 )
